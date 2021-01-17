d0a163446204b929d165556a2e9e65b28f9bdf65
arthursiq5 <arthur.siq5@gmail.com>
2021-01-17 13:36:45 -0300

    criada função pra desenhar a cobrinha

--- game.rb
+++ game.rb
@@ -1,6 +1,30 @@
 require 'gosu'
 
+class Snake
+    def initialize
+        @cells = [{ x: 0, y: 0 }, { x: 1, y: 0 }]
+    end
+
+    def draw window
+
+        cw = Game::CELL_WIDTH
+        width = Game::WIDTH
+        height = Game::HEIGHT
+
+        @cells.each do |cell|
+
+            window.draw_rect(cell[:x] * cw, cell[:y] * cw, cw, cw, Gosu::Color::BLACK)
+
+            window.draw_rect((cell[:x] * cw) + 1, (cell[:y] * cw) + 1, cw - 2, cw - 2, Gosu::Color::YELLOW)
+        
+        end
+
+    end
+
+end
+
 class Game < Gosu::Window
+    CELL_WIDTH = 20
     WIDTH = 800
     HEIGHT = 600
 
@@ -8,13 +32,14 @@ class Game < Gosu::Window
         super WIDTH, HEIGHT
         self.caption = "SnakeRB"
         @font = Gosu::Font.new(20)
+        @snake = Snake.new
     end
 
     def update
     end
 
     def draw
-        @font.draw_text 'olá, mundo', 10, 10, 10
+        @snake.draw self
     end
 
 end
